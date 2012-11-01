e6e75999329a4e937ccb87858f51111f0f566484
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2012-11-01 15:39:33 -0500

    Had to add in the year to the records, since it's not there in the log file.  Need this for the timestamp. Added nodeInfo.py

---
+++ python/lsst/ctrl/stats/nodeInfo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+from reader import Reader
+
+if __name__ == "__main__":
+    reader = Reader(sys.argv[1])
+    recordList = reader.getRecordList()
+    records = recordList.getRecords()
+
+    for job in records:
+        for rec in records[job]:
+            name = rec.__class__.__name__
+            if name == "Submitted":
+                print name
+                rec.printAll()
+            elif name == "Terminated":
+                print name
+                rec.printAll()
+        print "-----"
