99812db5483146faca62e1bf1006adee3cc99893
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2012-11-19 16:00:02 -0600

    Added argparse for arguments

--- bin/nodeInfo.py
+++ bin/nodeInfo.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 
+import argparse
 import sys
 from lsst.ctrl.stats.reader import Reader
 
 if __name__ == "__main__":
-    reader = Reader(sys.argv[1])
-    flag = sys.argv[2]
+
+    basename = os.path.basename(sys.argv[0])
+
+    parser = argparse.ArgumentParser(prog=basename)
+    parser.add_argument("-f", "--file", action="store", default=None, dest="filename", help="condor log file", type=str, required=True)
+    parser.add_argument("-c", "--complete", action="store_true", dest="complete", help="complete")
+
+    args = parser.parse_args()
+
+    reader = Reader(args.filename)
     recordList = reader.getRecordList()
     records = recordList.getRecords()
 
     for job in records:
         for rec in records[job]:
                 name = rec.__class__.__name__
-                if flag == "full":
+                if args.complete:
                     print name
                     rec.printAll()
                 else:
